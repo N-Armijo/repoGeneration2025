7902d8ea9bdffafcc4840775f863cf7a168cd8a1
narmijo-tj <toffee.jaguargmail.com>
2025-02-20 13:07:42 -0300

    Version do while no optimizada

--- src/genc182002/Bucles.java
+++ src/genc182002/Bucles.java
@@ -1,5 +1,7 @@
 package genc182002;
 
+import java.util.Scanner;
+
 public class Bucles {
     public static void main(String[] args) {
     //Bucle While
@@ -14,6 +16,24 @@ public class Bucles {
         System.out.println("Despegue");
 
     //Bucle Do-While -> permite qur unque la condicion sea falsa se ejecute al menos una vez
+        Scanner entrada = new Scanner(System.in);
+        Integer numeroMisterioso = (int) (Math.random() * 10) + 1; // -> recibe un double, pero lo casteo (transformo) a un int
+        Integer numeroElegido;
+        int contadorDeIntentos = 0;
+
+        do{
+            contadorDeIntentos++;
+            System.out.println("Ingrese un numero entero del 1 al 10: ");
+            String numero = entrada.nextLine();
+            numeroElegido = Integer.parseInt(numero); // -> hace un casteo de datos. Saca el numero del mensaje -> este metodo es una vuelta larga
+            if (numeroElegido == numeroMisterioso) {
+                System.out.println("Felicidades, adivinaste.\nNumero de intentos: " + contadorDeIntentos    );
+            } else if (numeroElegido < numeroMisterioso) {
+                System.out.println("El numero es mayor");
+            } else {
+                System.out.println("El numero es menor");
+            }
+        }while (numeroMisterioso != numeroElegido);
 
 
     }
