8a1fc4fb98a0d55b77d1720a079d4411c56689ae
narmijo-tj <toffee.jaguargmail.com>
2025-02-20 12:17:01 -0300

    Ciclo while en clase Bucles

--- src/genc182002/Bucles.java
+++ src/genc182002/Bucles.java
@@ -1,4 +1,17 @@
 package genc182002;
 
 public class Bucles {
+    public static void main(String[] args) {
+    //Bucle While
+    //Contador de despegue
+        int contador = 10;
+        System.out.println("Contador de despegue");
+
+        while (contador > 0) {
+            contador--;
+            System.out.println(contador);
+        }
+        System.out.println("Despegue");
+    }
+
 }
