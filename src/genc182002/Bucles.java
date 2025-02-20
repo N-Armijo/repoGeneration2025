e19c9261ea9538a48791a34e8ca703ed2d353afb
narmijo-tj <toffee.jaguargmail.com>
2025-02-20 12:23:11 -0300

    Se comenta el ciclo while y el uso del operador unario

--- src/genc182002/Bucles.java
+++ src/genc182002/Bucles.java
@@ -8,10 +8,14 @@ public class Bucles {
         System.out.println("Contador de despegue");
 
         while (contador > 0) {
-            contador--;
+            contador--; // -> indica que va restando 1 por cada iteracion del ciclo a la variable contador
             System.out.println(contador);
         }
         System.out.println("Despegue");
+
+    //Bucle Do-While -> permite qur unque la condicion sea falsa se ejecute al menos una vez
+
+
     }
 
 }
