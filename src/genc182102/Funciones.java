f85ccdda6dcfae2226ffdd39e1854305a619f0a8
narmijo-tj <singiru555@gmail.com>
2025-02-21 11:06:17 -0300

    Acceso de metoodos static desde la misma clase y diferencia de auqllos no definuidos como static en la clase

--- src/genc182102/Funciones.java
+++ src/genc182102/Funciones.java
@@ -1,5 +1,7 @@
 package genc182102;
 
+import java.util.Scanner;
+
 public class Funciones {
     //Modificadores de acceso
 
@@ -7,7 +9,13 @@ public class Funciones {
     public static  String segundoNombre;
     public String apellido;
     protected String segundoApellido;
+    public static void saludo(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese tu segundo nombre: ");
+        segundoNombre = sc.nextLine();
+        System.out.println("Hola, " + segundoNombre + "!");//Aqui solo puede acceder a lo que dice static. Sino lo tiene , La clase no sabe que le pertenece
+    }
     public static void main(String[] args) {
-
+        saludo(); //LLamo a la funcion
     }
 }
