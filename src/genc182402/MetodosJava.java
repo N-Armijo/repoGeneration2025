a623db4413598f2af2fed6cebc8c7ce3d543dee8
narmijo-tj <singiru555@gmail.com>
2025-02-24 11:01:51 -0300

    metodos para trabajar con fecha

--- src/genc182402/MetodosJava.java
+++ src/genc182402/MetodosJava.java
@@ -1,5 +1,8 @@
 package genc182402;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class MetodosJava {
 
     //metodo que trabaja con los metodos de la clase Math
@@ -25,8 +28,23 @@ public class MetodosJava {
         //LLamamos al metodo .equals() para comparar entre Strings
         //System.out.println("Los nombres " + nombreUno + " y " + nombreDos + " son iguales? " + (nombreUno == nombreDos) ); //Son objetos, tienen ubicacion en memoria distinta, no deberia funcionar. Por eso deberiamos usar el metodo de la clase String para comparar
         System.out.println("Los nombres " + nombreUno + " y " + nombreDos + " son iguales? " + nombreUno.equals(nombreDos) );
-    }
+        System.out.println("Los nombres " + nombreUno + " y " + nombreDos + " son iguales? " + nombreUno.equalsIgnoreCase(nombreDos) );
 
+        //Lamamos al etodo .length() para conocer la longitd de un String
+        System.out.println("Los nombres " + nombreUno + " y " + nombreDos + " suman una cantidad de " + (nombreUno+nombreDos).length() + " caracteres");
+
+        //Llamado al metodo .upperCase() para convertir el String a mayusculas
+        System.out.println("El primer nombre en mayuscula seria " + nombreUno.toUpperCase());
+        //llamamos al metodo lowerCase() para "Convertir" el string en minuscula -> crea un string nuevo a partir del dado, no los transforma
+        System.out.println("El primer nombre en minuscula seria " + nombreUno.toLowerCase());
+    }
+    //metodos que llame a metodos para trabajar con fchas
+    public static void llamadoMetodosFechas(){
+        //clase para almacenar fecha, inicializamos con fecha actual
+        LocalDate hoy = LocalDate.now();
+        //Para formatear de acuerdo al lugar donde vivo
+        DateTimeFormatter fechaFormateada = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    }
     public static void main(String[] args) {
         llamadoMetodosMath();
         llamadoMetodosString("Mariano" ,"MARIANO");
