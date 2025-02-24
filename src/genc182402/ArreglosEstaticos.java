a050f1ece8df79280f838a7647670091276ea11a
narmijo-tj <singiru555@gmail.com>
2025-02-24 12:32:39 -0300

    Bucle forEach

--- src/genc182402/ArreglosEstaticos.java
+++ src/genc182402/ArreglosEstaticos.java
@@ -1,6 +1,7 @@
 package genc182402;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArreglosEstaticos {
 
@@ -24,6 +25,22 @@ public class ArreglosEstaticos {
         System.out.println("Mi arreglo de numeros vacio que ya no esta vacio: " + Arrays.toString(miArregloVacio));
         System.out.println("Mi arreglo de numeros vacio que ya no esta vacio, accediendo con un indice " + miArregloVacio[0]);
 //        System.out.println("Mi arreglo de numeros vacio que ya no esta vacio, pero indicando un indice que no existe: " + miArregloVacio[6]);//Arrojara una exception ArrayIndexOutOfBoundsException
+
+        //Como puedo recorrer un arreglo?
+        Scanner sc = new Scanner(System.in);
+        int[] empty = new int[5];
+        for(int i = 0; i<empty.length; i++){
+            System.out.println("Ingrese un valor para el elemento en el indice " + i + ": ");
+            empty[i] = sc.nextInt();
+            sc.nextLine();
+            System.out.println("El numero en la posicion " + i + " ahora es " + empty[i]);
+        }
+        System.out.println("La lista de elementos son " + Arrays.toString(empty));
+
+        //ForEach es un tipo de bucle para trabajar con estructuras de datos. Se introdujo alrededor de java 5(?)-> confirmar dato
+        for(int elemento : empty){
+            System.out.println("Elemento en este momento tiene un valor de " + elemento);
+        }
 
     }
 }
